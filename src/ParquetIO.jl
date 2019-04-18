a33710dd7fa5a484aa2a35dbc816ce104f857789
FrancisH-C <francishchantal@gmail.com>
2019-04-18 13:01:16 -0400

    modified parquetIO

--- src/ParquetIO.jl
+++ src/ParquetIO.jl
@@ -23,7 +23,7 @@ input *.pqt, output a DataFrame
 """
 function import_pqt(pqt_file::String)
 	panda_df=Pandas.DataFrame(pd.read_parquet(pqt_file))
-	#panda_df ↦ julia_df : Pandas → DataFrames
+	# panda_df ↦ julia_df : Pandas → DataFrames
 	names=Symbol.(Pandas.values.(Pandas.columns(panda_df)))
 	df=DataFrames.DataFrame()
 	for i in 1:length(names)
@@ -57,73 +57,4 @@ function export_pqt(df, filename)
 end
 
 
-################################################################################
-# Testing
-################################################################################
-
-#--
-"""
-As  benchmark
-"""
-#function import_pqt_automatic(pqt_file)
-#	# DataFrames.DataFrame(panda_df)
-#	return DataFrames.DataFrame(Pandas.DataFrame(pd.read_parquet(pqt_file)))
-#end
-#--
-
-
-
-"""
-Importing
-"""
-#--
-#stock="HXT"
-#dates=readlines(`ls /data/banquenational/TSX/$stock/`)
-#@showprogress 1 "$stock" for day in dates[1:2]
-#	global trades=import_pqt("/data/banquenational/TSX/HXT/$day/trades.pqt")
-#	global orders=import_pqt("/data/banquenational/TSX/HXT/$day/orders.pqt")
-#	global orderbooks=import_pqt("/data/banquenational/TSX/HXT/$day/orderbooks.pqt")
-#end
-#--
-
-
-"""
-Exporting
-"""
-#-- 
-#export_pqt(trades, "test.pqt")
-#test=import_pqt("test.pqt")
-#--
-
-"""
-Benchmarking
-"""
-##-- 
-#TSX=readlines(`ls /data/banquenational/TSX/`)
-#t0=time()
-#stock="AAB"
-#for stock in TSX # really 4 min/stock
-#	dates=readlines(`ls /data/banquenational/TSX/$stock/`)
-#	@showprogress 1 "$stock" for day in dates[1:2]
-#		#global trades=import_pqt("/data/banquenational/TSX/HXT/$day/trades.pqt")
-#		global orders=import_pqt("/data/banquenational/TSX/HXT/$day/orders.pqt")
-#		#global orderbooks=import_pqt("/data/banquenational/TSX/HXT/$day/orderbooks.pqt")
-#	end
-#end
-
-#t1=time()
-#elapse=t1-t0
-#println(elapse)
-##--
-
-
-"""
-Sample of HXT 
-"""
-#--
-#using CSV
-#CSV.write("trades.csv", first(trades,6))
-#CSV.write("orders.csv", first(orders,10))
-#CSV.write("orderbooks.csv", first(orderbooks,10))
-#--
 end # module
