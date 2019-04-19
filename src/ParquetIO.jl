37dc876d7bbe694b5a37912f11a59e2ede6c803f
FrancisH-C <francishchantal@gmail.com>
2019-04-19 10:26:59 -0400

    still debugging

--- src/ParquetIO.jl
+++ src/ParquetIO.jl
@@ -26,6 +26,7 @@ input *.pqt, output a DataFrame
 function import_pqt(pqt_file::String)
 	println(pqt_file)
 	#panda_df=Pandas.DataFrame(pd.read_parquet(pqt_file))
+	return Pandas.DataFrame(pd.read_parquet(pqt_file))
 	# panda_df ↦ julia_df : Pandas → DataFrames
 	names=Symbol.(Pandas.values.(Pandas.columns(panda_df)))
 	df=DataFrames.DataFrame()
