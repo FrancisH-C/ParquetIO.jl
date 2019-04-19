809ba11a3e6da509d8246d02d52856057b345456
FrancisH-C <francishchantal@gmail.com>
2019-04-19 10:48:11 -0400

    I think I understand now

--- src/ParquetIO.jl
+++ src/ParquetIO.jl
@@ -5,8 +5,6 @@ export import_pqt, export_pqt
 #--
 using DataFrames, PyCall, Suppressor, ProgressMeter
 import Pandas
-pd=pyimport("pandas")
-#pushfirst!(PyVector(pyimport("sys")."path"), "") # from PyCall doc
 #--
 
 
@@ -26,9 +24,7 @@ input *.pqt, output a DataFrame
 function import_pqt(pqt_file::String)
 	pd=pyimport("pandas")
 	println(pqt_file)
-	#panda_df=Pandas.DataFrame(pd.read_parquet(pqt_file))
-	pd.read_parquet(pqt_file)
-	return 1
+	panda_df=Pandas.DataFrame(pd.read_parquet(pqt_file))
 	# panda_df ↦ julia_df : Pandas → DataFrames
 	names=Symbol.(Pandas.values.(Pandas.columns(panda_df)))
 	df=DataFrames.DataFrame()
