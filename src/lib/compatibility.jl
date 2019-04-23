a5e2965bc30b1b47fffc9b7e472c4f0d7e001e59
FrancisH-C <francishchantal@gmail.com>
2019-04-23 10:56:15 -0400

    some day I'll understand markdown

--- src/lib/compatibility.jl
+++ src/lib/compatibility.jl
@@ -1,7 +1,8 @@
 """
 `isnothing(x::Any)`
 
-because isnothing exsits in 1.1 but not in 1.0
+Because isnothing exsits in 1.1 but not in 1.0
+
 return true if value is nothing and false otherwise
 """
 function isnothing(x::Any)
