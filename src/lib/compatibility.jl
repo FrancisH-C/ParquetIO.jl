1c35283bb99bc4a6fce6919a3b8d834110c6c28a
FrancisH-C <francishchantal@gmail.com>
2019-04-23 10:51:05 -0400

    doc for isnothing

--- src/lib/compatibility.jl
+++ src/lib/compatibility.jl
@@ -1,4 +1,8 @@
-# because isnothing exsits in 1.1 but not in 1.0
+"""
+`isnothing(x::Any)`
+because isnothing exsits in 1.1 but not in 1.0
+return true if value is nothing and false otherwise
+"""
 function isnothing(x::Any)
 	return x == nothing
 end
