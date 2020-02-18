b8f214d97c20baddf626594bd4fc440d57d1c8de
FrancisH-C <francishchantal@gmail.com>
2020-02-18 10:17:49 -0500

    file permisson bug

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,14 +5,15 @@ using Test, ParquetIO, CSV, DataFrames
 pqt="test.pqt"
 csv="test.csv"
 df=CSV.read(csv)
+df=CSV.write(csv)
 
 # Int128
-df[!,end] = convert.(Int128, df[:,end])
-df[!,end] = df[:,end].^2
-
-export_pqt(pqt, df)
-df=import_pqt(pqt)
-
-csv2pqt(csv, pqt)
-pqt2csv(pqt, csv)
+#df[!,end] = convert.(Int128, df[:,end])
+#df[!,end] = df[:,end].^2
+#
+#export_pqt(pqt, df)
+#df=import_pqt(pqt)
+#
+#csv2pqt(csv, pqt)
+#pqt2csv(pqt, csv)
 end
