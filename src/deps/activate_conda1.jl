f1950812e0df5846590b56ffd159a0251867f3ca
FrancisH-C <francishchantal@gmail.com>
2020-03-24 14:00:03 -0400

    working on build

--- src/deps/activate_conda1.jl
+++ src/deps/activate_conda1.jl
@@ -1,5 +1,5 @@
 using Pkg
-ENV["CONDA_JL_HOME"]="$JULIA_DEPOT_PATH/conda/3/"
+ENV["CONDA_JL_HOME"]="$(DEPOT_PATH[1])/conda/3/"
 Pkg.build("Conda")
 import Conda
 Conda.runconda(`create --name PyCall --file conda_requirements.txt`)
--- src/deps/activate_conda2.jl
+++ src/deps/activate_conda2.jl
@@ -1,5 +1,5 @@
 using Pkg
-ENV["CONDA_JL_HOME"]="$JULIA_DEPOT_PATH/conda/3/envs/PyCall"
+ENV["CONDA_JL_HOME"]="$(DEPOT_PATH[1])/conda/3/envs/PyCall"
 Pkg.build("Conda");
 Pkg.build("PyCall");
 using Conda
